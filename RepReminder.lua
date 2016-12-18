5c6c0bad1d97b7a6f59f77511cba0792fd3aae7d
Jord <jordanleekent@hotmail.com>
2016-12-18 02:40:48 +0000

    Add files via upload

---
+++ RepReminder.lua
@@ -0,0 +1,7 @@
+local Congrats_EventFrame = CreateFrame("Frame")
+Congrats_EventFrame:RegisterEvent("UPDATE_FACTION")
+Congrats_EventFrame:SetScript("OnEvent",
+	function(self, event, ...)
+		local arg1, arg2, = ...
+		print('Reminder to Reach Exalted with The NightFallen or you will struggle late game in Legion ' .. arg1 .. ', ' .. UnitName("Player") .. '! DON'T ' .. arg2 .. ' FORGET ' .. arg3 .. ' PLEASE')
+	end)
