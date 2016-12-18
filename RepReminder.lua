8f299690053f11c744fb0db65a4b8b3ec93d1a5a
Jord <jordanleekent@hotmail.com>
2016-12-18 02:43:55 +0000

    Update RepReminder.lua

--- RepReminder.lua
+++ RepReminder.lua
@@ -2,6 +2,6 @@ local Congrats_EventFrame = CreateFrame("Frame")
 Congrats_EventFrame:RegisterEvent("UPDATE_FACTION")
 Congrats_EventFrame:SetScript("OnEvent",
 	function(self, event, ...)
-		local arg1, arg2, = ...
+		local arg1, arg2, arg3 = ...
 		print('Reminder to Reach Exalted with The NightFallen or you will struggle late game in Legion ' .. arg1 .. ', ' .. UnitName("Player") .. '! DO NOT ' .. arg2 .. ' FORGET ' .. arg3 .. ' PLEASE')
 	end)
